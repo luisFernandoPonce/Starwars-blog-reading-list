6d410e0e9051be6ca3cc10dba8cd4c9e04144284
Alejandro <eadelgadom@gmail.com>
2022-04-27 02:24:41 +0000

    funcionando people

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -4,52 +4,62 @@ import {
 	ListGroupItem,
 	Pagination,
 	PaginationItems,
+	PaginationNext,
+	PaginationPrev,
+	Button,
 } from "react-bootstrap";
 import { people } from "../apiStarWars.js";
 
 const List = () => {
-	var [data, setData] = useState([
-		{
-			uid: "1",
-			name: "Luke Skywalker",
-			url: "https://www.swapi.tech/api/people/1",
-		},
-		{
-			uid: "2",
-			name: "C-3PO",
-			url: "https://www.swapi.tech/api/people/2",
-		},
-		{
-			uid: "3",
-			name: "R2-D2",
-			url: "https://www.swapi.tech/api/people/3",
-		},
-		{
-			uid: "4",
-			name: "Darth Vader",
-			url: "https://www.swapi.tech/api/people/4",
-		},
-		{
-			uid: "5",
-			name: "Leia Organa",
-			url: "https://www.swapi.tech/api/people/5",
-		},
-	]);
-
+	var [data, setData] = useState([]);
 	var [page, setPage] = useState(1);
-	var [pageNext, setPageNext] = useState(2);
-	var [pagePrevious, setPagePrevious] = useState(0);
-	var [pages, setPages] = useState(10);
+	var [pages, setPages] = useState(1); //me indica el numero de paginas del footer list
 	var [paginationItems, setPaginationItems] = useState([]);
 
-	people.getById(1).then((data) => console.log(data));
-	people.getQuery().then((data) => console.log(data));
+	function irAPagina(id) {
+		people.getQuery(id).then((data) => {
+			console.log("cargando pagina...", id);
+			// Se actualizan los valores del estado
+			setData(data.results);
+			setPage(id);
+			// Esta actualizacion tiene un hook
+			setPages(data.total_pages);
+			console.log("Cargada pagina ", id);
+		});
+	}
+
+	function siguientePagina() {
+		if (page < pages);
+		irAPagina(page + 1);
+	}
+
+	function previaPagina() {
+		if (1 < page) {
+			irAPagina(page - 1);
+		}
+	}
+
+	//people.getById(1).then((data) => console.log(data));
+	//people.getQuery().then((data) => console.log(data));
 
 	useEffect(() => {
+		console.log("Componente montado");
+		irAPagina(1);
+		return () => {
+			console.log("Componente desmontado");
+		};
+	}, []);
+
+	useEffect(() => {
+		console.log("Actualizando paginas");
 		actualizarPaginacion();
+		return () => {
+			console.log("Finalizada la actualizacion de paginas");
+		};
 	}, [page, pages]);
 
 	function getItems() {
+		if (!data) return;
 		return data.map((person) => {
 			return (
 				<ListGroup.Item key={person.uid}>{person.name}</ListGroup.Item>
@@ -57,17 +67,6 @@ const List = () => {
 		});
 	}
 
-	function siguientePagina() {
-		if (page < pages);
-		setPage(page + 1);
-	}
-	function irAPagina(id) {
-		setPage(id);
-	}
-	function previaPagina() {
-		if (0 > page);
-		setPage(page - 1);
-	}
 	function actualizarPaginacion() {
 		var tmp = [];
 		for (var i = 1; i <= pages; i++) {
@@ -97,4 +96,5 @@ const List = () => {
 		</div>
 	);
 };
+
 export default List;
